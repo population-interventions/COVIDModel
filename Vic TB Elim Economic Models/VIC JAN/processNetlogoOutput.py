40f5f9ee3df83cda969b3ec7b1ef8b00ef01ebe8
Tim Wilson <timedwilson@gmail.com>
2021-03-31 10:00:03 +1100

    Heatmap code now supports automatic sorting.

--- Vic TB Elim Economic Models/VIC JAN/processNetlogoOutput.py
+++ Vic TB Elim Economic Models/VIC JAN/processNetlogoOutput.py
@@ -189,9 +189,40 @@ def AverageRuns(directory, file):
     df = df.groupby(level=[2, 3, 4, 5, 6, 7, 8], axis=0).mean()
     
     OutputToFile(df, directory + file + '_average_runs', 'aa')
-    df = df[[str(i + 14) + '.0' for i in range(26)]]
+    df = df[[str(i + 27) + '.0' for i in range(13)]]
     df = df.transpose().describe().transpose()
-    OutputToFile(df, directory + file + '_stage2_means', 'aaa')
+    
+    df = df[['mean']]
+    df = df.reset_index()
+    df = df[df['param_vac1_tran_reduct'] == df['param_vac2_tran_reduct']]
+    df = df.drop(columns=['param_vac2_tran_reduct', 'global_transmissibility'])
+    df = df.rename(columns={'param_vac1_tran_reduct' : 'param_vac_tran_reduct'})
+    
+    df['param_vac_uptake'] = df['param_vac_uptake'].replace({
+        60 : '60',
+        75 : '075',
+        90 : '0090',
+    })
+    
+    df['sort'] = df['param_policy'].replace({
+        'AggressElim' : 'a',
+        'ModerateElim' : 'b',
+        'TightSupress' : 'c',
+        'LooseSupress' : 'd'
+    }) + df['param_vac_tran_reduct'].replace({
+        90 : 'a',
+        75 : 'b',
+        50 : 'c',
+    })
+        
+    df = df.set_index(['param_policy', 'param_vac_tran_reduct', 'R0',
+                       'param_trigger_loosen', 'param_vac_uptake', 'sort'])
+    df = df.unstack(['sort', 'param_policy', 'param_vac_tran_reduct'])
+    
+    df = df.sort_index(axis=1, level=0)
+    df.columns = df.columns.droplevel(level=1)
+    
+    OutputToFile(df, directory + file + '_stage2_infections_mean', 'stage2b_only_sort')
     
 
 def AverageRunsStages(directory, file):
@@ -201,7 +232,7 @@ def AverageRunsStages(directory, file):
     df = df.groupby(level=[2, 3, 4, 5, 6, 7, 8], axis=0).mean()
     
     df = df.droplevel([0, 2], axis=1)
-    df = df[[str(i + 81) for i in range(181)]]
+    df = df[[str(i + 182) for i in range(81)]]
     OutputToFile(df, directory + file + '_stage2_stages_mean', 'test')
     df = df.transpose().describe().transpose()
     df = df[['mean']]
@@ -215,23 +246,37 @@ def AverageRunsStages(directory, file):
         75 : '075',
         90 : '0090',
     })
+    df['sort'] = df['param_policy'].replace({
+        'AggressElim' : 'a',
+        'ModerateElim' : 'b',
+        'TightSupress' : 'c',
+        'LooseSupress' : 'd'
+    }) + df['param_vac_tran_reduct'].replace({
+        90 : 'a',
+        75 : 'b',
+        50 : 'c',
+    })
     
     df = df.set_index(['param_policy', 'param_vac_tran_reduct', 'R0',
-                       'param_trigger_loosen', 'param_vac_uptake'])
-    df = df.unstack(['param_policy', 'param_vac_tran_reduct'])
-    OutputToFile(df, directory + file + '_stage2_stages_mean', 'aaa')
+                       'param_trigger_loosen', 'param_vac_uptake', 'sort'])
+    df = df.unstack(['sort', 'param_policy', 'param_vac_tran_reduct'])
+    
+    df = df.sort_index(axis=1, level=0)
+    df.columns = df.columns.droplevel(level=1)
+    
+    OutputToFile(df, directory + file + '_stage2_stages_mean', 'stage2b_only')
 
 
 directory = 'Output/runTry5/'
-ProcessRawOutput(directory + 'processed',
-    [directory + 'mergedresult']
-    )
-##RemoveDuplicates(directory + 'processed_infectNoVac')
-##RemoveDuplicates(directory + 'processed_infectVac')
-##RemoveDuplicates(directory + 'processed_stage')
-ProcessFileToVisualisation(directory + 'processed', 'infectNoVac') 
-ProcessFileToVisualisation(directory + 'processed', 'infectVac')
-AddFiles(directory, 'processed_infectNoVac_weeklyAgg',
-    'processed_infectVac_weeklyAgg', 'processed_infect_unique', 'weeklyAgg')
+#ProcessRawOutput(directory + 'processed',
+#    [directory + 'mergedresult']
+#    )
+###RemoveDuplicates(directory + 'processed_infectNoVac')
+###RemoveDuplicates(directory + 'processed_infectVac')
+###RemoveDuplicates(directory + 'processed_stage')
+#ProcessFileToVisualisation(directory + 'processed', 'infectNoVac') 
+#ProcessFileToVisualisation(directory + 'processed', 'infectVac')
+#AddFiles(directory, 'processed_infectNoVac_weeklyAgg',
+#    'processed_infectVac_weeklyAgg', 'processed_infect_unique', 'weeklyAgg')
 AverageRuns(directory, 'processed_infect_unique_weeklyAgg')
-AverageRunsStages(directory, 'processed_stage')
+#AverageRunsStages(directory, 'processed_stage')
